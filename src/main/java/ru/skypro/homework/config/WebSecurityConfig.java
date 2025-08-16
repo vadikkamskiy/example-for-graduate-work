cf38a2f2105f1bb243e849382c6dc58dccdb9ae9
vadikkamskiy <vadikkamskiy@gmail.com>
2025-08-16 15:07:44 +0300

    configurate project and all

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -3,15 +3,15 @@ package ru.skypro.homework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.skypro.homework.dto.Role;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import ru.skypro.homework.dto.Role;
 
 @Configuration
 public class WebSecurityConfig {
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
             "/swagger-ui.html",
-            "/v3/api-docs",
+            "/v3/api-docs/**",
             "/webjars/**",
             "/login",
             "/register"
@@ -27,30 +27,25 @@ public class WebSecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user =
-                User.builder()
-                        .username("user@gmail.com")
-                        .password("password")
-                        .passwordEncoder(passwordEncoder::encode)
-                        .roles(Role.USER.name())
-                        .build();
+        UserDetails user = User.builder()
+                .username("user@gmail.com")
+                .password(passwordEncoder.encode("password"))
+                .roles(Role.USER.name())
+                .build();
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests(
-                        authorization ->
-                                authorization
-                                        .mvcMatchers(AUTH_WHITELIST)
-                                        .permitAll()
-                                        .mvcMatchers("/ads/**", "/users/**")
-                                        .authenticated())
-                .cors()
-                .and()
-                .httpBasic(withDefaults());
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(AUTH_WHITELIST).permitAll()
+                    .requestMatchers("/ads/**", "/users/**").authenticated()
+                    .anyRequest().denyAll()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .cors(cors -> {});
         return http.build();
     }
 
@@ -58,5 +53,4 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
